77193146364370b7c94267fb5ff4136a5ff564c0
lukas.droste <lukas.droste@vocanto.de>
2021-09-26 16:58:31 +0200

    init ws server

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-app.get('/', function (_req, res) {
-    res.send('Hello world!');
-});
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-    console.log("Listening on port " + PORT);
+var ws_1 = require("ws");
+var wss = new ws_1.WebSocketServer({ port: 3000 });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    ws.send('something');
 });
 //# sourceMappingURL=server.js.map
