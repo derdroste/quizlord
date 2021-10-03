3aa957362a8e706e0ab7a65557a121e68a7f17c5
David Koch <david_koch1@web.de>
2021-10-03 13:28:15 +0200

    add matchmaking queue loading dialog

---
+++ app/lib/components/matchmaking_loading_dialog.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class MatchmakingLoadingDialog extends StatefulWidget {
+  final Function onClose;
+
+  const MatchmakingLoadingDialog({Key? key, required this.onClose})
+      : super(key: key);
+
+  @override
+  State<MatchmakingLoadingDialog> createState() =>
+      _MatchmakingLoadingDialogState();
+}
+
+class _MatchmakingLoadingDialogState extends State<MatchmakingLoadingDialog>
+    with SingleTickerProviderStateMixin {
+  var _animationController;
+
+  var _colorTween;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    _colorTween = _animationController
+        .drive(ColorTween(begin: Colors.yellow, end: Colors.blue));
+    _animationController.repeat(reverse: true);
+    super.initState();
+  }
+
+  dialogContent(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            offset: Offset(0.0, 10.0),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // To make the card compact
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(
+                valueColor: _colorTween,
+                backgroundColor: Colors.grey,
+                strokeWidth: 10,
+              )),
+          const SizedBox(height: 16.0),
+          TextButton(
+            onPressed: () => {widget.onClose.call(), Navigator.pop(context)},
+            child: const Text(
+              "Cancel",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
--- app/lib/home_screen.dart
+++ app/lib/home_screen.dart
@@ -1,48 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_lord/components/matchmaking_loading_dialog.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _MyHomeScreenState();
 }
 
 class _MyHomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
+  late WebSocketChannel _channel;
+  bool _loading = false;
+  bool _connected = false;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    _startChannelConnection();
+    super.initState();
+  }
+
+  void _startChannelConnection() {
+    if (!_connected) {
+      _channel = WebSocketChannel.connect(
+        Uri.parse('ws://quizlord-22rsv.ondigitalocean.app/:3000/'),
+      );
+      _connected = true;
+      print('Channel connection started');
+    }
+  }
+
+  void _closeChannelConnection() {
+    _channel.sink.close();
+    print('Channel connection closed');
+    _connected = false;
+  }
+
+  void _dismissMatchmakingDialog() {
+    _closeChannelConnection();
+  }
+
+  void _startMatchmaking() {
+    _startChannelConnection();
+    _channel.sink.add('Hello from Flutter!');
     setState(() {
-      _counter++;
+      //  _loading = true;
     });
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return MatchmakingLoadingDialog(onClose: _dismissMatchmakingDialog);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: _startMatchmaking,
+                  child: const Text('Playbuddne'),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12), // <-- Radius
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    _closeChannelConnection();
+    super.dispose();
   }
 }
